5acc8637fc4386a6c39fcff5b70ac8b63cf6fed4
jp calvo <calvojp92@gmail.com>
2023-05-30 17:42:40 +0800

    add tooltip

--- packages/chakra/src/theme/components/tooltip.ts
+++ packages/chakra/src/theme/components/tooltip.ts
@@ -7,17 +7,33 @@ const $arrowBg = cssVar("popper-arrow-bg");
 
 export const Tooltip: StyleConfig = {
   baseStyle: {
-    [$fgColor.variable]: colors.neutral[100],
-    [$bgColor.variable]: colors.neutral[900],
-    [$arrowBg.variable]: $bgColor.reference,
-
     py: 2,
     px: 3,
-    color: $fgColor.reference,
-    bgColor: $bgColor.reference,
     rounded: 2,
-    fontSize: "sm",
+    fontSize: "12px",
     fontWeight: "medium",
     lineHeight: "18px",
+    color: $fgColor.reference,
+    bgColor: $bgColor.reference,
+  },
+  variants: {
+    dark: {
+      [$fgColor.variable]: colors.neutral[100],
+      [$bgColor.variable]: colors.neutral[900],
+      [$arrowBg.variable]: $bgColor.reference,
+    },
+    light: {
+      [$fgColor.variable]: colors.gray[700],
+      [$bgColor.variable]: colors.white,
+      [$arrowBg.variable]: $bgColor.reference,
+      border: "1px",
+      borderColor: colors.gray[100],
+      boxShadow:
+        "0px 12px 16px -4px rgba(16, 24, 40, 0.08)," +
+        "0px 4px 6px -2px rgba(16, 24, 40, 0.03)",
+    },
+  },
+  defaultProps: {
+    variant: "dark",
   },
 };
