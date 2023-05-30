7325727cb29a53998ba3a3bf2c824c9292bb68db
jp calvo <calvojp92@gmail.com>
2023-05-30 22:50:38 +0800

    add pagination context

--- packages/chakra/src/Pagination/Pagination.tsx
+++ packages/chakra/src/Pagination/Pagination.tsx
@@ -1,3 +1,31 @@
-export function Pagination() {
-  return null;
-}
+import {
+  chakra,
+  HTMLChakraProps,
+  omitThemingProps,
+  ThemingProps,
+  useMultiStyleConfig,
+} from "@chakra-ui/react";
+import * as React from "react";
+import { Merge } from "../types";
+import { PaginationStylesProvider } from "./PaginationContext";
+
+export type PaginationProps = Merge<
+  HTMLChakraProps<"div"> & ThemingProps<"Pagination">,
+  {}
+>;
+
+export const Pagination = React.forwardRef<HTMLDivElement, PaginationProps>(
+  function Pagination(props, ref) {
+    const styles = useMultiStyleConfig("Pagination", props);
+
+    const { children, ...others } = omitThemingProps(props);
+
+    return (
+      <chakra.div ref={ref} __css={styles.container} {...others}>
+        <PaginationStylesProvider value={styles}>
+          {children}
+        </PaginationStylesProvider>
+      </chakra.div>
+    );
+  },
+);
