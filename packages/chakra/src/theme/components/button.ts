3b033027dc063c0896e2777fc21d3ba71ffbce0f
jp calvo <calvojp92@gmail.com>
2023-05-30 18:15:33 +0800

    x

--- packages/chakra/src/theme/components/button.ts
+++ packages/chakra/src/theme/components/button.ts
@@ -5,45 +5,80 @@ export const Button: StyleConfig = {
     rounded: 1,
   },
   variants: {
-    solid({ colorScheme }) {
+    solid({ theme, colorScheme }) {
       return {
         color: "neutral.100",
-        fontWeight: "medium",
         transition: "all 300ms ease-in-out",
         boxShadow: "0px 1px 2px rgba(16, 24, 40, 0.05)",
         bgColor: `${colorScheme}.700`,
         _hover: {
-          bgColor: `${colorScheme}.700`,
+          bgColor: `${colorScheme}.900`,
         },
         _focus: {
-          boxShadow: "none",
-          bgColor: `${colorScheme}.800`,
+          boxShadow:
+            "0px 1px 2px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px " +
+            theme.colors[colorScheme][100],
+        },
+        _disabled: {
+          bgColor: `${colorScheme}.500`,
+          opacity: 1,
         },
       };
+    },
+    outline() {
+      return {};
+    },
+    subtle() {
+      return {};
+    },
+    ghost() {
+      return {};
+    },
+    link() {
+      return {};
     },
   },
   sizes: {
     sm: {
-      height: 9,
-      paddingY: 2,
-      paddingX: 3.5,
-      fontSize: "xs",
+      h: 9,
+      py: 2,
+      px: 3.5,
+      fontSize: "12px",
+      fontWeight: "medium",
       lineHeight: "12px",
       letterSpacing: "0.02em",
     },
     md: {
-      height: 10,
-      paddingY: 2.5,
-      paddingX: 4,
-      fontSize: "sm",
+      h: 10,
+      py: 2.5,
+      px: 4,
+      fontSize: "14px",
+      fontWeight: "semibold",
       lineHeight: "14px",
       letterSpacing: "0.02em",
     },
     lg: {
-      height: 11,
-      paddingY: 2.5,
-      paddingX: "18px",
-      fontSize: "md",
+      h: 11,
+      py: 2.5,
+      px: 4.5,
+      fontSize: "18px",
+      fontWeight: "semibold",
+      lineHeight: "24px",
+    },
+    xl: {
+      h: 12,
+      py: 3,
+      px: 5,
+      fontSize: "20px",
+      fontWeight: "semibold",
+      lineHeight: "20px",
+    },
+    "2xl": {
+      h: 13,
+      py: 4,
+      px: 7,
+      fontSize: "18px",
+      fontWeight: "semibold",
       lineHeight: "24px",
     },
   },
