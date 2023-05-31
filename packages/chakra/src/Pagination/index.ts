0b33aed31b8263c8a566900e1a7698ef446da044
jp calvo <calvojp92@gmail.com>
2023-05-31 12:29:35 +0800

    fix export

--- packages/chakra/src/Pagination/index.ts
+++ packages/chakra/src/Pagination/index.ts
@@ -2,6 +2,6 @@ export * from "./Pagination";
 export * from "./PaginationNextTrigger";
 export * from "./PaginationPageTrigger";
 export * from "./PaginationPrevTrigger";
+export * from "./PaginationRange";
 export * from "./PaginationSizeControl";
-export * from "./PaginationSummary";
 export * from "./PaginationTriggers";
