54e6deb4193bf6dffb3f3e22e4e9ec61b9fcc11a
jp calvo <calvojp92@gmail.com>
2023-05-31 11:17:57 +0800

    add extend

--- packages/examples/react/src/app.tsx
+++ packages/examples/react/src/app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { theme } from "@scaleforge-ui/chakra";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Landing } from "./landing";
@@ -8,7 +8,25 @@ import { Login } from "./login";
 export default function App() {
   return (
     <ChakraProvider
-      theme={theme}
+      theme={extendTheme(theme, {
+        /*
+        colors: {
+          primary: {
+            50: "#f0f9ff",
+            100: "#e0f2fe",
+            200: "#bae6fd",
+            300: "#7dd3fc",
+            400: "#38bdf8",
+            500: "#0ea5e9",
+            600: "#0284c7",
+            700: "#0369a1",
+            800: "#075985",
+            900: "#0c4a6e",
+            950: "#082f49",
+          },
+        },
+        */
+      })}
       toastOptions={{
         defaultOptions: {
           status: "success",
